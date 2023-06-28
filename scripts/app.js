a8b5b7d082b4fcb71fb8f7451044f07afff37e6f
shahin-hossain <shahinhossain540@gmail.com>
2023-06-28 18:49:34 +0600

    site interactivity added

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+//player selection function
 function playerSelect(playerId, btnId) {
     const player = document.getElementById(playerId);
     const PlayerList = document.getElementById('player-list');
@@ -10,6 +11,7 @@ function playerSelect(playerId, btnId) {
     disabledButton.style.backgroundColor = 'gray';
     disabledButton.innerText = 'SECECTED';
 }
+//over five selection disabled function
 function selectedPlayer(playerId, btnPlayerId) {
     const orderList = document.getElementById('player-list').childElementCount;
 
@@ -20,6 +22,28 @@ function selectedPlayer(playerId, btnPlayerId) {
     else {
         playerSelect(playerId, btnPlayerId)
     }
+}
+//Get input Field Value Funcation
+
+function getInputFieldValue(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldString);
+    return inputFieldValue;
+
+}
+//Get Element Field Value Funcation
+function getElementFieldValue(ElementId) {
+    const elementField = document.getElementById(ElementId);
+    const elementFieldString = elementField.innerText;
+    const elementFieldValue = parseFloat(elementFieldString);
+
+    return elementFieldValue;
+}
+//Element Set Funcation 
+function setElementValue(elementId, value) {
+    const elementField = document.getElementById(elementId);
+    elementField.innerText = value;
 }
 document.getElementById('btn-player-1').addEventListener('click', function () {
     selectedPlayer('player-1', 'btn-player-1')
@@ -39,19 +63,38 @@ document.getElementById('btn-player-5').addEventListener('click', () => {
 document.getElementById('btn-player-6').addEventListener('click', () => {
     selectedPlayer('player-6', 'btn-player-6')
 })
-/* 
-function playerSelect(playerId) {
-    const player = document.getElementById(playerId);
-    const PlayerList = document.getElementById('player-list');
 
-    const li = document.createElement('li');
-    li.innerText = player.innerText;
-    PlayerList.appendChild(li);
+function getPlayerExpanses() {
+    const playerNumber = document.getElementById('player-list').childElementCount;
+    const perPlayer = getInputFieldValue('per-player-field');
+    const playersCharge = playerNumber * perPlayer;
+
+    if (isNaN(playersCharge)) {
+        alert('Please give Number Value');
+    }
+    else {
+        setElementValue('player-expenses', playersCharge)
+    }
 }
-const btnSelects = document.getElementsByClassName('btn-select');
-for (const btnSelect of btnSelects) {
-    btnSelect.addEventListener('click', function () {
-        playerSelect('player-1')
-        playerSelect('player-2')
-    })
-} */
+//total caculate funcction 
+function totalCostCalculate() {
+    const playerCharge = getElementFieldValue('player-expenses');
+    const managerCharge = getInputFieldValue('manager-field');
+    const coachCharge = getInputFieldValue('coach-field');
+
+    const totalCost = playerCharge + managerCharge + coachCharge;
+
+    if (isNaN(totalCost)) {
+        alert('Please give number value')
+    }
+    else {
+        setElementValue('total-cost', totalCost)
+    }
+}
+document.getElementById('btn-calculate').addEventListener('click', () => {
+    getPlayerExpanses()
+})
+
+document.getElementById('btn-total-calculate').addEventListener('click', () => {
+    totalCostCalculate()
+})
